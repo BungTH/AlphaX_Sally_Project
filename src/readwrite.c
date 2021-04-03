fd85e530f4f61979aeba854d5f638da183cc5c2c
BungkapTH <www.bung555@gmail.com>
2021-04-04 00:09:34 +0700

    Update readwrite.c for implement of strtok - stuck at linked list

--- src/readwrite.c
+++ src/readwrite.c
@@ -20,44 +20,12 @@
 #include "readwrite.h"
 
 #define ALLTAG 512
-
-
-
-void readData(PHOTO_T* pHead, HASHITEM_T* hashphoto[], HASHTAG_T* hashtag[])
-{
-    FILE* pIn = NULL;
-
-    PHOTO_T* inputData;
-    
-    char pic_name[PHOTOSIZE];
-    int pic_tagNum;
-    char pic_path[PATHSIZE];
-    char pic_tagAll[ALLTAG];
-
-    char inputline[320];
-
-    pIn = fopen("tag.txt","r");
-    while(fgets(inputline,sizeof(inputline),pIn) != NULL)
-    {
-        inputData = calloc(1,sizeof(PHOTO_T));
-        if(inputData == NULL)
-        {
-            printf("\tError - Calloc failed for inputData\n");
-        }
-        else
-        {
-            sscanf(inputline,"%s:%d:%s",pic_name,pic_tagNum,pic_path);
-            fgets(inputline,sizeof(inputline),pIn);
-            sscanf(inputline,"%s",pic_tagAll);
-            printf("%s - %d - %s\n%s",pic_name,pic_tagNum,pic_path,pic_tagAll);
-        }
-    }
-}
+#define BUFFER 512
 
 
 
 //use dtype.c
-void add_photo_2_hashphoto(PHOTO_T* photo, HASHITEM_T* hashphoto[])
+void add_photo_2_hashphoto(PHOTO_T * photo, HASHITEM_T * hashphoto[])
 {
 
 }
@@ -65,7 +33,7 @@ void add_photo_2_hashphoto(PHOTO_T* photo, HASHITEM_T* hashphoto[])
 
 
 //use dtype.c
-void add_photo_2_hashtag(PHOTO_T* photo, HASHITEM_T* hashtag[])
+void add_photo_2_hashtag(PHOTO_T * photo, HASHITEM_T * hashtag[])
 {
 
 }
@@ -73,7 +41,7 @@ void add_photo_2_hashtag(PHOTO_T* photo, HASHITEM_T* hashtag[])
 
 
 //use "next" and hand build link list
-void add_photo_2_masterlist(PHOTO_T* photo, PHOTO_T* pHead)
+void add_photo_2_masterlist(PHOTO_T * photo, PHOTO_T * pHead)
 {
 
 }
@@ -81,7 +49,7 @@ void add_photo_2_masterlist(PHOTO_T* photo, PHOTO_T* pHead)
 
 
 //set data to NULL
-void freeAll(PHOTO_T* pHead, HASHITEM_T* hashphoto[], HASHITEM_T* hashtag[])
+void freeAll(PHOTO_T * pHead, HASHITEM_T * hashphoto[], HASHITEM_T * hashtag[])
 {
 
 }
@@ -89,7 +57,7 @@ void freeAll(PHOTO_T* pHead, HASHITEM_T* hashphoto[], HASHITEM_T* hashtag[])
 
 
 //write data to
-void writeData(PHOTO_T* pHead)
+void writeData(PHOTO_T * pHead)
 {
 
 }
@@ -103,7 +71,85 @@ void createFile()
 
 
 
+void readData(PHOTO_T * pHead, HASHITEM_T * hashphoto[], HASHTAG_T * hashtag[])
+{
+    FILE * pIn = NULL;
+
+    PHOTO_T * inputData;
+
+    LIST_TAG_T * pListHead = NULL;
+    LIST_TAG_T * pListTemp = NULL;
+
+    char pic_name[PHOTOSIZE];
+    int pic_tagNum;
+    char pic_path[PATHSIZE];
+    char pic_tagAll[ALLTAG];
+
+    const char delim[] = ";";
+    char * ptr = NULL;
+
+    char inputline[BUFFER];
+
+    pIn = fopen("tag.txt","r");
+    if(pIn == NULL)
+    {
+        printf("\tERROR - can't open file\n");
+    }
+    else
+    {
+        while(fgets(inputline,sizeof(inputline),pIn) != NULL)
+        {
+            inputData = calloc(1,sizeof(PHOTO_T));
+            if(inputData == NULL)
+            {
+                printf("\tERROR - Calloc failed for inputData\n");
+            }
+            else
+            {
+                sscanf(inputline,"%[^;];%d;%s",pic_name,&pic_tagNum,pic_path);
+                fgets(inputline,sizeof(inputline),pIn);
+                sscanf(inputline,"%s",pic_tagAll);
+                //printf("%s - %d - %s - %s",pic_name,pic_tagNum,pic_path,pic_tagAll);
+                strcpy(inputData->namephoto,pic_name);
+                inputData->numtag = pic_tagNum;
+                strcpy(inputData->path,pic_path);
+                //1st strtok
+                pListHead = calloc(1,sizeof(LIST_TAG_T));
+                pListTemp = calloc(1,sizeof(LIST_TAG_T));
+                ptr = strtok(pic_tagAll,delim);
+                //other strtok 
+                while(ptr != NULL)
+                {
+                    printf("ptr = %s\n",ptr);
+                    /*strcpy(pListTemp->nametag,ptr); //core dump
+                    if(pListHead == NULL)
+                    {
+                        pListHead = pListTemp;
+                    }
+                    else
+                    {
+                        pListTemp->next = pListHead;
+                        pListHead = pListTemp;
+                    }*/
+                    ptr = strtok(NULL,delim);
+                }
+                //strtok then assign tag
+                /*add_photo_2_hashphoto(inputData, hashphoto);
+                add_photo_2_hashtag(inputData, hashtag);
+                add_photo_2_masterlist(inputData, pHead);*/
+            }
+        }
+        fclose(pIn);
+    }
+}
+
+
+
 int main()
 {
+    PHOTO_T* dummyHead;
+    HASHITEM_T** dummyhashphoto;
+    HASHTAG_T** dummyhashtag;
 
+    readData(dummyHead,dummyhashphoto,dummyhashtag);
 }
